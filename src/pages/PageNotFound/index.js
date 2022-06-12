fed19f32c4b1cabd4db4793625cc070472e90708
Adel-Abdallah <adelgafo@gmail.com>
2022-06-12 22:23:49 +0800

    feat: not-found-page added

--- src/pages/PageNotFound/index.js
+++ src/pages/PageNotFound/index.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./index.css";
+import { useNavigate } from "react-router-dom";
 
 const PageNotFound = () => {
+  const navigate = useNavigate();
+  const handleClick = data => {
+    console.log(data);
+    navigate("/", {
+      state: data
+    });
+  };
   return (
     <div className='not-found-container'>
-      <div className=''>
-        <h1>404 Error</h1>
-        <h1>Page Not Found</h1>
-      </div>
+      <span className='num'>404</span>
+      <span>Page Not Found</span>
+      <p>the page you are looking for is not seem to exist</p>
+      <button className='button' onClick={handleClick}>
+        {" "}
+        Go To Home Page
+      </button>
     </div>
   );
 };
